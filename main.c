d618575c96398a53cdb2dde18120dccf9193ef24
Marco Kurzynski <marcokurzynski@icloud.com>
2022-04-28 03:11:31 -0500

    better demo

--- main.c
+++ main.c
@@ -1,40 +1,3 @@
-/*
- * Copyright (c) 2016-2019, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
- *  ======== main_freertos.c ========
- */
-
-/* RTOS header files */
 #include "serial_debug.h"
 #include "main.h"
 #include "msp.h"
@@ -53,13 +16,9 @@
 
 TaskHandle_t TaskH_joystick = NULL;
 TaskHandle_t TaskH_newFrame = NULL;
+TaskHandle_t TaskH_s2 = NULL;
 SemaphoreHandle_t Sem_LCD = NULL;
 
-
-/* ****************************************************************************
- * This Function initializes the hardware required to blink LED1 on the
- * MSP432 Launchpad
- * ***************************************************************************/
 static uint8_t x = 64, y = 64;
 
 inline void init(void)
@@ -69,21 +28,30 @@ inline void init(void)
 
 	  Crystalfontz128x128_Init();
 
-    t32_init();
     adc14_init();
 	  i2c_init();
 	  opt3001_init();
 	  serial_debug_init();
 }
 
-/******************************************************************************
-* Tasked used to blink LED1 on MSP432 Launchpad
-******************************************************************************/
+void Task_s2(void *pvParameters) {
+	while(true) {
+    static uint8_t buttonState = 0;
+    buttonState = buttonState << 1 | MKII_S2();
+
+    if (buttonState == 0x7F) S2_P = true;
+    ADC14->CTL0 |= ADC14_CTL0_SC | ADC14_CTL0_ENC;
+    TIMER32_1->INTCLR = BIT0;
+		vTaskDelay(pdMS_TO_TICKS(7));
+	}
+}
+
 void Task_newFrame(void *pvParameters)
 {
 	while(true) {
     enum light {DARK,MEDIUM,BRIGHT,foo};
     static enum light l, pl = foo; // so pl != l
+    vTaskDelay(pdMS_TO_TICKS(3));
 	  float lux = opt3001_read_lux();
 		if (lux < 20) l=DARK;
 		else if (lux < 75) l=MEDIUM;
@@ -102,8 +70,9 @@ void Task_newFrame(void *pvParameters)
 			}
 		}
 		pl = l;
-    vTaskDelay(pdMS_TO_TICKS(10));
 		draw_crosshair(x,y);
+		draw_crosshair(64,64);
+		//draw_crosshair(0,0);
 		draw_moving_layers();
 	}
 }
@@ -114,14 +83,11 @@ void Task_joystick(void *pvParameters) {
 	  else if (PS2_Y_VAL == PS2_DIR_DOWN) y+= 1;
 	  if (PS2_X_VAL == PS2_DIR_LEFT) x-= 1;
 	  else if (PS2_X_VAL == PS2_DIR_RIGHT) x+= 1;
-    vTaskDelay(pdMS_TO_TICKS(5));
+    vTaskDelay(pdMS_TO_TICKS(17));
 	}
 }
 
 
-/*
- *  ======== main ========
- */
 int main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
@@ -130,7 +96,8 @@ int main(void)
     Sem_LCD = xSemaphoreCreateBinary();
     xSemaphoreGive(Sem_LCD);
     xTaskCreate(Task_newFrame, "newFrame", configMINIMAL_STACK_SIZE, NULL, 1, &TaskH_newFrame);
-    xTaskCreate(Task_joystick, "joystick", configMINIMAL_STACK_SIZE, NULL, 2, &TaskH_joystick);
+    xTaskCreate(Task_joystick, "joystick", configMINIMAL_STACK_SIZE, NULL, 4, &TaskH_joystick);
+    xTaskCreate(Task_s2, "s2", configMINIMAL_STACK_SIZE, NULL, 4, &TaskH_s2);
 
     vTaskStartScheduler();
 
